271c00b170f4b63a9707b629d83e134a41a424fb
jheinz1999 <jonathan.heinz@aol.com>
2019-02-01 16:37:02 -0800

    set up users table

---
+++ data/20190201162311_users.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', tbl => {
+
+    // primary key: id
+    tbl.increments();
+
+    // other keys
+
+    tbl.string('username').unique().notNullable();
+    tbl.string('email').unique().notNullable();
+    tbl.string('password').notNullable();
+    tbl.string('image_url').notNullable();
+
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('users');
+};

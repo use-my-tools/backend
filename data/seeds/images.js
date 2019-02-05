61fe45bf6c38bab13ca280f06a06ead1844e7973
jheinz1999 <jonathan.heinz@aol.com>
2019-02-05 15:44:46 -0800

    added seeds for tools

--- data/seeds/images.js
+++ data/seeds/images.js
@@ -5,7 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('images').insert([
-        {id: 1, url: 'https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png'}
+        {id: 1, url: 'https://www.qualiscare.com/wp-content/uploads/2017/08/default-user.png'},
+        {id: 2, url: 'https://www.wildetool.com/wp-content/uploads/2017/02/085432002112.png'},
+        {id: 3, url: 'https://cdn.shopify.com/s/files/1/1996/9367/products/910053_800x.jpg'},
+        {id: 4, url: 'https://www.aerospecialties.com/app/uploads/2017/05/RBI9900TM-and-RBA2-isolated_01.jpg'},
       ]);
     });
 };

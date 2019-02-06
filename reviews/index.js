8211281dd20f515ef2ab62434f34e243aa134288
jheinz1999 <jonathan.heinz@aol.com>
2019-02-06 08:34:21 -0800

    reviews route now sends back all users

--- reviews/index.js
+++ reviews/index.js
@@ -5,6 +5,28 @@ const db = require('../data/db');
 
 const server = express.Router();
 
+returnAllUsers = async res => {
+
+  let users = await db.select('u.firstname', 'u.lastname', 'u.id', 'u.username', 'i.url').from('users as u').join('images as i', 'i.id', 'u.image_id').paginate(10, 1, true);
+
+  const results = users.data.map(async (user) => {
+
+    const reviews = await db.select().from('reviews').where({ for_user: user.id });
+
+    user.reviews = reviews;
+    user.stars = 0.0 + reviews.reduce((acc, val) => acc += val.stars, 0) / reviews.length || 0;
+
+    return user;
+
+  });
+
+  Promise.all(results).then(completed => {
+    users.data = completed;
+    res.status(200).json(users);
+  });
+
+}
+
 server.post('/', authenticate, async (req, res) => {
 
   const { review, stars, for_user } = req.body;
@@ -38,7 +60,7 @@ server.post('/', authenticate, async (req, res) => {
 
     await db.insert({ review, stars, for_user, by_user }).into('reviews');
 
-    res.status(201).json({message: 'Review published!'});
+    returnAllUsers(res);
 
   }
 
